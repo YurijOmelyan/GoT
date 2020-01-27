f5333acd22ea00b2ca4623145d5d1445a9aaf777
Yuriy Omelyan <omelyan.yuriy@gmail.com>
2020-01-27 21:58:08 +0200

    First review PS3

--- js/main.js
+++ js/main.js
@@ -5,7 +5,11 @@ $(document).ready(function() {
     $("select")
       .selectBoxIt({
         autoWidth: false,
-        defaultText: "Select House"
+        showFirstOption: false,
+        showEffect: "fadeIn",
+        showEffectSpeed: 400,
+        hideEffect: "fadeOut",
+        hideEffectSpeed: 400
       })
       .change(function() {
         $(".flexslider").flexslider(this.value - 1);
